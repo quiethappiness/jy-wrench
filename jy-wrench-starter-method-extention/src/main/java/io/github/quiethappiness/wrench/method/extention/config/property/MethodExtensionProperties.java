ef4480b33d434c8b8591239ec6785675e5ae9e05
jingyue <jingyue20220129@163.com>
2025-10-07 22:17:40 +0800

    fix(config):修正配置属性前缀
    - 将配置前缀从 "jy.wrench.methode" 更改为 "jy.wrench.config.methode"- 保持忽略无效字段的配置不变

--- jy-wrench-starter-method-extention/src/main/java/io/github/quiethappiness/wrench/method/extention/config/property/MethodExtensionProperties.java
+++ jy-wrench-starter-method-extention/src/main/java/io/github/quiethappiness/wrench/method/extention/config/property/MethodExtensionProperties.java
@@ -3,7 +3,7 @@ package io.github.quiethappiness.wrench.method.extention.config.property;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "jy.wrench.methode", ignoreInvalidFields = true)
+@ConfigurationProperties(prefix = "jy.wrench.config.methode", ignoreInvalidFields = true)
 @Data
 public class MethodExtensionProperties
 {
